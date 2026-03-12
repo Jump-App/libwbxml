fe61884f3f6468b28eb0ef6837abfa237f3b0fb9
Brian Petersen <bp@jumpapp.com>
2026-03-12 14:21:49 -0600

    Adsd checksum.exs for 0.1.0 manually

---
+++ checksum.exs
@@ -0,0 +1,5 @@
+%{
+  "libwbxml-nif-2.17-aarch64-apple-darwin-0.1.0.tar.gz" => "sha256:4dcdba2d9339132e6489c9f27973fc61ee640084425bdfad389353a661e8297b",
+  "libwbxml-nif-2.17-x86_64-apple-darwin-0.1.0.tar.gz" => "sha256:1acb6d3be0945105701aa52388b118dff83c16281df0476925253463a37e34b3",
+  "libwbxml-nif-2.17-x86_64-linux-gnu-0.1.0.tar.gz" => "sha256:29ff07bb2c299363c8d730a00e04cce9c340d575f90e982d9ac1122707474324",
+}
