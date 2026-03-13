798949ce9784e1154517dbd7f7e3f3c5fd74a6f2
Brian Petersen <bp@jumpapp.com>
2026-03-13 10:00:32 -0600

    Updates checksum

--- checksum.exs
+++ checksum.exs
@@ -1,5 +1,5 @@
 %{
-  "libwbxml-nif-2.17-aarch64-apple-darwin-0.1.0.tar.gz" => "sha256:4dcdba2d9339132e6489c9f27973fc61ee640084425bdfad389353a661e8297b",
-  "libwbxml-nif-2.17-x86_64-apple-darwin-0.1.0.tar.gz" => "sha256:1acb6d3be0945105701aa52388b118dff83c16281df0476925253463a37e34b3",
-  "libwbxml-nif-2.17-x86_64-linux-gnu-0.1.0.tar.gz" => "sha256:29ff07bb2c299363c8d730a00e04cce9c340d575f90e982d9ac1122707474324",
+  "libwbxml-nif-2.17-aarch64-apple-darwin-0.1.0.tar.gz" => "sha256:72fd4a1b4ae273040425cbf89a4cb767ece056cde47f43f0d3afebd8e81765d2",
+  "libwbxml-nif-2.17-x86_64-apple-darwin-0.1.0.tar.gz" => "sha256:1b451d56188807030fec9d4706067cdde367f44231e43061090c1aed082a58b2",
+  "libwbxml-nif-2.17-x86_64-linux-gnu-0.1.0.tar.gz" => "sha256:c72699175dc9b9ef22d97125328c68c5cfecebd224d3ac7555e8d3167580e627",
 }
